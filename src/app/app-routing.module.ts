c811c0fc92d2d39decc18ffb62b36ac9cd5d2312
Elio Tombolesi <elio_tombo_2@hotmail.com>
2022-09-06 17:49:24 -0300

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '', redirectTo: '', pathMatch: 'full' //Para el home
+  },
+  {
+    path: '**', redirectTo: '', pathMatch: 'full' //Para el 404
+  }/*,
+  {
+    path: '404-not-found',
+    loadChildren: ()=> import('@public/pages/not-found/not-found.module').then(m => m.NotFoundModule)
+  }*/
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
