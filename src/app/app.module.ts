65f3d00e77bf5f97625cf4ce578600861c1d975c
Elio Tombolesi <elio_tombo_2@hotmail.com>
2022-09-13 20:04:36 -0300

    commit1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { RegisterComponent } from './public/pages/register/register.component';
+import { HeaderComponent } from './public/components/header/header.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    RegisterComponent
+    RegisterComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/public/pages/lista-contactos/lista-contactos.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { ListaContactosRoutingModule } from './lista-contactos-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    ListaContactosRoutingModule
+  ]
+})
+export class ListaContactosModule { }
