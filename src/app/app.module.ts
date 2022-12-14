b41a75b8916858a146dcac5af62233e272b77e88
mateomonti1 <mateomonti1@gmail.com>
2022-12-14 13:30:54 -0300

    Elegir dispositivo y eliminé delete-contacts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,7 +6,6 @@ import { AppComponent } from './app.component';
 
 import { HeaderComponent } from './public/components/header/header.component';
 import { ListaContactosComponent } from './public/pages/lista-contactos/lista-contactos.component';
-import { DeleteContactsComponent } from './public/pages/delete-contacts/delete-contacts.component';
 import { NewContactsComponent } from './public/pages/new-contacts/new-contacts.component';
 import { SignInComponent } from './public/pages/sign-in/sign-in.component';
 import { EditContactComponent } from './public/pages/edit-contact/edit-contact.component';
@@ -16,9 +15,7 @@ import { EditContactComponent } from './public/pages/edit-contact/edit-contact.c
     AppComponent,
     HeaderComponent,
      ListaContactosComponent,
-     DeleteContactsComponent,
      NewContactsComponent,
-     
   ],
   imports: [
     BrowserModule,
